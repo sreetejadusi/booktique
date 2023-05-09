32a10d8d5e8198c0aa86d971be22670a1287f9af
sreetejadusi <sreetejadusi@gmail.com>
2023-05-09 18:15:05 +0530

    Basic Funtionality

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
+import requests
+from bs4 import BeautifulSoup
 my_list = []
 def home(request):
     my_list.clear()
@@ -7,5 +9,15 @@ def home(request):
 
 
 def update_list(request):
-    my_list.append(f'{len(my_list)+1}')
+    query = request.GET.get('query')
+    page = int(request.GET.get('page'))
+    filetypes = ['pdf','docx','ipub','xlsx','csv','ppt']
+    url = f'https://google.com/search?q={query}&start={10*page}&num=10'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+    links = soup.find_all('a', href=True)
+    my_list= [str(x['href']) for x in links if str(x['href']).endswith('pdf')]
+    print(my_list)
     return JsonResponse({'my_list': my_list})
